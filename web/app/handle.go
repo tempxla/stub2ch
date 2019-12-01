9d3c05833110647ed7a078aeb58111039c73c6ee
tempxla <38803749+tempxla@users.noreply.github.com>
2019-12-01 10:42:30 +0900

    handle dat

--- web/app/handle.go
+++ web/app/handle.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 )
 
 var (
@@ -34,6 +35,24 @@ func handleBbsCgi(w http.ResponseWriter, r *http.Request, ps httprouter.Params) 
 	fmt.Fprintf(w, "bbs.cgi, %s!\n", board)
 }
 
+func handleDat(sv *service.BoardService) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		board := ps.ByName("board")
+		threadKey := strings.Replace(ps.ByName("dat"), ".dat", "", 1)
+		dat, err := sv.MakeDat(board, threadKey)
+		if err != nil {
+			if err == datastore.ErrNoSuchEntity {
+				http.Error(w, "Not found", http.StatusNotFound)
+			} else {
+				log.Printf("ERROR: handleDat. %v", err)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+			}
+			return
+		}
+		fmt.Fprintf(w, dat)
+	}
+}
+
 func handleSubjectTxt(sv *service.BoardService) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		board := ps.ByName("board")
@@ -53,8 +72,4 @@ func handleSubjectTxt(sv *service.BoardService) httprouter.Handle {
 
 func handleSettingTxt(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "setting.txt, %s!\n", ps.ByName("board"))
-}
-
-func handleDat(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "dat, %s %s!\n", ps.ByName("board"), ps.ByName("dat"))
 }
