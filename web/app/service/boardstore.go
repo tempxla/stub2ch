aef3add1d07813a5ad77d1a6a3bc963531df36cb
tempxla <38803749+tempxla@users.noreply.github.com>
2019-12-09 16:36:14 +0900

    トランザクション実装

--- web/app/service/boardstore.go
+++ web/app/service/boardstore.go
@@ -12,29 +12,46 @@ type BoardStore struct {
 }
 
 func (repo *BoardStore) GetBoard(key *datastore.Key, entity *BoardEntity) (err error) {
-	if err = repo.Client.Get(repo.Context, key, entity); err != nil {
-		return
-	}
+	err = repo.Client.Get(repo.Context, key, entity)
 	return
 }
 
 func (repo *BoardStore) PutBoard(key *datastore.Key, entity *BoardEntity) (err error) {
-	if _, err := repo.Client.Put(repo.Context, key, entity); err != nil {
-		return err
-	}
+	_, err = repo.Client.Put(repo.Context, key, entity)
 	return
 }
 
 func (repo *BoardStore) GetDat(key *datastore.Key, entity *DatEntity) (err error) {
-	if err = repo.Client.Get(repo.Context, key, entity); err != nil {
-		return
-	}
+	err = repo.Client.Get(repo.Context, key, entity)
 	return
 }
 
 func (repo *BoardStore) PutDat(key *datastore.Key, entity *DatEntity) (err error) {
-	if _, err := repo.Client.Put(repo.Context, key, entity); err != nil {
-		return err
-	}
+	_, err = repo.Client.Put(repo.Context, key, entity)
+	return
+}
+
+func (repo *BoardStore) RunInTransaction(f func(tx *datastore.Transaction) error) (err error) {
+	_, err = repo.Client.RunInTransaction(repo.Context, f)
+	return
+}
+
+func (repo *BoardStore) TxGetBoard(tx *datastore.Transaction, key *datastore.Key, entity *BoardEntity) (err error) {
+	err = tx.Get(key, entity)
+	return
+}
+
+func (repo *BoardStore) TxPutBoard(tx *datastore.Transaction, key *datastore.Key, entity *BoardEntity) (err error) {
+	_, err = tx.Put(key, entity)
+	return
+}
+
+func (repo *BoardStore) TxGetDat(tx *datastore.Transaction, key *datastore.Key, entity *DatEntity) (err error) {
+	err = tx.Get(key, entity)
+	return
+}
+
+func (repo *BoardStore) TxPutDat(tx *datastore.Transaction, key *datastore.Key, entity *DatEntity) (err error) {
+	_, err = tx.Put(key, entity)
 	return
 }
