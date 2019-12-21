89b770ba90287c6a19fd9b3c9d9e1f000cb116d4
tempxla <38803749+tempxla@users.noreply.github.com>
2019-12-21 15:10:52 +0900

    add tests

--- internal/app/handle/handle_test.go
+++ internal/app/handle/handle_test.go
@@ -1,6 +1,8 @@
 package handle
 
 import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"github.com/tempxla/stub2ch/internal/app/service"
 	"net/http"
 	"net/http/httptest"
@@ -25,5 +27,55 @@ func TestHandleIndex(t *testing.T) {
 	// Verify
 	if err := indexTmpl.Execute(writer, nil); err != nil {
 		t.Errorf("Error executing template: %v", err)
+	}
+}
+
+func TestProtect(t *testing.T) {
+	// Setup
+	handleOK := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		fmt.Fprintf(w, "OK")
+		return
+	}
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/", nil)
+
+	// Exercise
+	router := httprouter.New()
+	router.GET("/", protect(true)(handleOK))
+	router.ServeHTTP(writer, request)
+
+	// Verify
+	if writer.Code != 503 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+	body := writer.Body.String()
+	if body == "OK" {
+		t.Error("body is OK.")
+	}
+}
+
+func TestProtect_NotProtect(t *testing.T) {
+	// Setup
+	handleOK := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		fmt.Fprintf(w, "OK")
+		return
+	}
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/", nil)
+
+	// Exercise
+	router := httprouter.New()
+	router.GET("/", protect(false)(handleOK))
+	router.ServeHTTP(writer, request)
+
+	// Verify
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+	body := writer.Body.String()
+	if body != "OK" {
+		t.Errorf("body is %v", body)
 	}
 }
